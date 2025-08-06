4e5279c869407b4e9877f3c237b31181ea0d02be
Cees Melis <cees3331x00@gmail.com>
2025-08-07 00:27:42 +0200

    Print armor

--- Common.h
+++ Common.h
@@ -10,9 +10,15 @@ namespace Archetype
     {
         BYTE x00[0x18];
         UINT idsInfo; // 0x18, full stats
-        BYTE x1C[0xD0];
+        float hitPts; // 0x1C
+        BYTE x20[0xCC];
         UINT idsInfo1; // 0xEC, name and description
         UINT idsInfo2; // 0xF0, stat names
         UINT idsInfo3; // 0xF4, stat values
+        BYTE xF8[0x8];
+        float holdSize; // 0x100
+        BYTE x104[0x3C];
+        int nanobotLimit; // 0x140
+        int shieldBatteryLimit; // 0x144
     };
 }
