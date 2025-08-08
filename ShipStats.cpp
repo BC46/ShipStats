b251acb76314d1762c25b5f567cec8a407be2466
Cees Melis <cees3331x00@gmail.com>
2025-08-09 00:46:27 +0200

    Print additional equipment stat

--- ShipStats.cpp
+++ ShipStats.cpp
@@ -4,31 +4,6 @@
 #include <stdio.h>
 #include <set>
 
-int GetMaximumWeaponClass(const Archetype::Ship& shipArch)
-{
-    int result = 1;
-
-    for (UINT i = 0; i < shipArch.hpTypes.size(); ++i)
-    {
-        if (shipArch.hpTypes[i].id >= HPTYPEID_GUN_SPECIAL_1 && shipArch.hpTypes[i].id <= HPTYPEID_GUN_SPECIAL_10)
-            result = max(result, shipArch.hpTypes[i].id - HPTYPEID_GUN_SPECIAL_1 + 1);
-        else if (shipArch.hpTypes[i].id >= HPTYPEID_TURRET_SPECIAL_1 && shipArch.hpTypes[i].id <= HPTYPEID_TURRET_SPECIAL_10)
-            result = max(result, shipArch.hpTypes[i].id - HPTYPEID_TURRET_SPECIAL_1 + 1);
-    }
-
-    return result;
-}
-
-void PrintOptimalWeaponClassStat(LPWSTR buffer, const Archetype::Ship& shipArch)
-{
-    swprintf(buffer, L"%d", max(GetMaximumWeaponClass(shipArch) - 2, 1));
-}
-
-void PrintMaximumWeaponClassStat(LPWSTR buffer, const Archetype::Ship& shipArch)
-{
-    swprintf(buffer, L"%d", GetMaximumWeaponClass(shipArch));
-}
-
 void AddHardpointIdsToSet(std::set<UINT> &set, const st6::vector<LPCSTR>& hardpoints)
 {
     for (UINT i = 0; i < hardpoints.size(); ++i)
@@ -65,17 +40,61 @@ void PrintBatsBotsStat(LPWSTR buffer, const Archetype::Ship& shipArch)
     swprintf(buffer, L"%d/%d", shipArch.shieldBatteryLimit, shipArch.nanobotLimit);
 }
 
+int GetMaximumWeaponClass(const Archetype::Ship& shipArch)
+{
+    int result = 1;
+
+    for (UINT i = 0; i < shipArch.hpTypes.size(); ++i)
+    {
+        if (shipArch.hpTypes[i].id >= HPTYPEID_GUN_SPECIAL_1 && shipArch.hpTypes[i].id <= HPTYPEID_GUN_SPECIAL_10)
+            result = max(result, shipArch.hpTypes[i].id - HPTYPEID_GUN_SPECIAL_1 + 1);
+        else if (shipArch.hpTypes[i].id >= HPTYPEID_TURRET_SPECIAL_1 && shipArch.hpTypes[i].id <= HPTYPEID_TURRET_SPECIAL_10)
+            result = max(result, shipArch.hpTypes[i].id - HPTYPEID_TURRET_SPECIAL_1 + 1);
+    }
+
+    return result;
+}
+
+void PrintOptimalWeaponClassStat(LPWSTR buffer, const Archetype::Ship& shipArch)
+{
+    swprintf(buffer, L"%d", max(GetMaximumWeaponClass(shipArch) - 2, 1));
+}
+
+void PrintMaximumWeaponClassStat(LPWSTR buffer, const Archetype::Ship& shipArch)
+{
+    swprintf(buffer, L"%d", GetMaximumWeaponClass(shipArch));
+}
+
+void PrintAdditionalEquipmentStat(LPWSTR buffer, const Archetype::Ship& shipArch)
+{
+    bool m = false, cm = false, cd = false, t = false;
+
+    for (UINT i = 0; i < shipArch.hpTypes.size(); ++i)
+    {
+        m   |= shipArch.hpTypes[i].id == HPTYPEID_MINE_DROPPER;
+        cm  |= shipArch.hpTypes[i].id == HPTYPEID_COUNTERMEASURE_DROPPER;
+        cd  |= shipArch.hpTypes[i].id == HPTYPEID_TORPEDO_SPECIAL_2;
+        t   |= shipArch.hpTypes[i].id == HPTYPEID_TORPEDO_SPECIAL_1;
+    }
+
+    LPCWSTR mStr = m ? ((cm | cd | t) ? L"M, " : L"M") : L"";
+    LPCWSTR cmStr = cm ? ((cd | t) ? L"CM, " : L"CM") : L"";
+    LPCWSTR cdtStr = (cd & t) ? L"CD/T" : (cd ? L"CD" : (t ? L"T" : L""));
+    swprintf(buffer, L"%s%s%s", mStr, cmStr, cdtStr);
+}
+
 void AppendShipInfo_Inventory_Hook(const Archetype::Ship& shipArch, RenderDisplayList& rdl)
 {
     // TODO: Make name strings configurable (localizations).
     static const ShipStat shipStats[] =
     {
-        { L"Gun/Turret Mounts", PrintGunTurretMountsStat },
-        { L"Armor", PrintArmorStat },
-        { L"Cargo Space", PrintCargoSpaceStat },
-        { L"Max. Batteries/Nanobots", PrintBatsBotsStat },
-        { L"Optimal Weapon Class", PrintOptimalWeaponClassStat },
-        { L"Maximum Weapon Class", PrintMaximumWeaponClassStat }
+        { L"Gun/Turret Mounts",         PrintGunTurretMountsStat },
+        { L"Armor",                     PrintArmorStat },
+        { L"Cargo Space",               PrintCargoSpaceStat },
+        { L"Max. Batteries/Nanobots",   PrintBatsBotsStat },
+        { L"Optimal Weapon Class",      PrintOptimalWeaponClassStat },
+        { L"Maximum Weapon Class",      PrintMaximumWeaponClassStat },
+        { L"Additional Equipment",      PrintAdditionalEquipmentStat }
     };
 
     // Some ships in vanilla FL (e.g. capships) only have a description as infocard, so make sure this is printed.
